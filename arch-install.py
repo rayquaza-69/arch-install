8f2ab7a059e15360a03a076ba8c0fb7a73adc384
rayquaza-69 <advaymakhija@gmail.com>
2022-03-18 20:35:56 +0530

    test

--- arch-install.py
+++ arch-install.py
@@ -57,7 +57,7 @@ else:
 print("Configuring time.")
 run("ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime",shell=True)
 
-pacinstall(ntpd)
+pacinstall('ntpd')
 
 run("hwclock --systohc",shell=True)
 
